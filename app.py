41e891591b871408106b9f37424ee305dcf2512a
fant1-LB <fantinpro84@gmail.com>
2025-06-05 16:58:54 +0200

    Création de la BDD

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.debug=True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+from models.database import *
+
+def init_db():
+    db.create_all()
+    
 
 
-@app.route('/')
-def check():
-    return 'Flask fonctionne'
 
 if __name__ == '__main__':
+    with app.app_context():
+        init_db()
     app.run()
---
+++ models/database.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+db= SQLAlchemy(app)
+
+class ObjetDuFonds(db.Model):
+    __tablename__ ='ObjetDuFonds'
+
+    id = db.Column(db.Text(30), primary_key= True, unique=True)
+    id_pour_les_AN = db.Column(db.Text(30))
+    id_nature = db.Column(db.Text(30))
+    id_contenant_orig = db.Column(db.Text(30), db.ForeignKey('PochettesForbin.id'))
+    id_contenant_contemporrain = db.Column(db.Text(30), db.ForeignKey('CartonsAN.id'))
+
+
+class PochettesForbin(db.Model):
+    __tablename__="PochettesForbin"
+    id=db.Column(db.Text(30), primary_key=True, unique=True)
+    titre= db.Column(db.Text(50))
+    natureDuContenant = db.Column(db.Text(30))
+
+class CartonsAN(db.Model):
+    __tablename__ = "CartonsAN"
+    id=db.Column(db.Text(30), primary_key=True, unique=True)
+    titre= db.Column(db.Text(50))
+    
+
